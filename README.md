d97c98aad086de0c70960e288bc48b84a5a07b0f
Viral B. Shah <viral@mayin.org>
2015-04-08 00:16:09 +0530

    Basic Yeppp library wrapper.

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
-# Yeppp
+Yeppp bindings for Julia
+========================
 
-[![Build Status](https://travis-ci.org/ViralBShah/Yeppp.jl.svg?branch=master)](https://travis-ci.org/ViralBShah/Yeppp.jl)
+[Yeppp!](http://www.yeppp.info) is a high-performance SIMD-optimized
+mathematical library. This Julia package makes it possible to
+call Yeppp from Julia.
+
+To use this, make sure `libyeppp` is available on the system library
+search path or in the current directory.
+
+See example usage below, where Yeppp's vectorized log is 7x faster
+than the one in Base that uses [openlibm](http://www.openlibm.org/).
+
+```julia
+using Yeppp
+
+x = rand(10^7)
+ty = @elapsed Yeppp.log!(similar(x), x)
+t  = @elapsed log(x)
+t/ty
+````
+
+The following functions are available. Inputs are in `x`, and outputs
+are in `y`.
+```julia
+dot(x1, x2)
+sum(x)
+sumsq(x)
+log!(y, x)
+exp!(y, x)
+sin!(y, x)
+cos!(y, x)
+tan!(y, x)
+````
--- src/Yeppp.jl
+++ src/Yeppp.jl
@@ -1,5 +1,78 @@
 module Yeppp
 
-# package code goes here
+function __init__()
+    const status = ccall( (:yepLibrary_Init, "libyeppp"), Int32, ())
+    status != 0 && error("yepLibrary_Init: error: ", status)
+end
+
+function release()
+    const status = ccall( (:yepLibrary_Release, "libyeppp"), Int32, ())
+    status != 0 && error("yepLibrary_Release: error: ", status)
+end
+
+function sum(v::AbstractVector{Float64})
+    n = length(v)
+    local s::Vector{Float64} = Array(Float64, 1)
+    const status = ccall( (:yepCore_Sum_V64f_S64f, "libyeppp"), Int32, (Ptr{Float64}, Ptr{Float64}, Uint), v, s, n)
+    status != 0 && error("yepCore_Sum_V64f_S64f: error: ", status)
+    s[1]
+end
+
+function sumsq(v::AbstractVector{Float64})
+    n = length(v)
+    s = Array(Float64, 1)
+    const status = ccall( (:yepCore_SumSquares_V64f_S64f, "libyeppp"), Int32, (Ptr{Float64}, Ptr{Float64}, Uint), v, s, n)
+    status != 0 && error("yepCore_SumSquares_V64f_S64f: error: ", status)
+    s[1]
+end
+
+function dot(x::AbstractVector{Float64}, y::AbstractVector{Float64})
+    assert(length(x) == length(y))
+    n = length(x)
+    dotproduct = Array(Float64, 1)
+    const status = ccall( (:yepCore_DotProduct_V64fV64f_S64f, "libyeppp"), Int32, (Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Uint), x, y, dotproduct, n)
+    status != 0 && error("yepCore_DotProduct_V64fV64f_S64f: error: ", status)
+    dotproduct[1]
+end
+
+function log!(y::AbstractVector{Float64}, x::AbstractVector{Float64})
+    assert(length(x) == length(y))
+    n = length(x)
+    const status = ccall( (:yepMath_Log_V64f_V64f, "libyeppp"), Int32, (Ptr{Float64}, Ptr{Float64}, Uint), x, y, n)
+    status != 0 && error("yepMath_Log_V64f_V64f: error: ", status)
+    y
+end
+
+function exp!(y::AbstractVector{Float64}, x::AbstractVector{Float64})
+    assert(length(x) == length(y))
+    n = length(x)
+    const status = ccall( (:yepMath_Exp_V64f_V64f, "libyeppp"), Int32, (Ptr{Float64}, Ptr{Float64}, Uint), x, y, n)
+    status != 0 && error("yepMath_Exp_V64f_V64f: error: ", status)
+    y
+end
+
+function sin!(y::AbstractVector{Float64}, x::AbstractVector{Float64})
+    assert(length(x) == length(y))
+    n = length(x)
+    const status = ccall( (:yepMath_Sin_V64f_V64f, "libyeppp"), Int32, (Ptr{Float64}, Ptr{Float64}, Uint), x, y, n)
+    status != 0 && error("yepMath_Sin_V64f_V64f: error: ", status)
+    y
+end
+
+function cos!(y::AbstractVector{Float64}, x::AbstractVector{Float64})
+    assert(length(x) == length(y))
+    n = length(x)
+    const status = ccall( (:yepMath_Cos_V64f_V64f, "libyeppp"), Int32, (Ptr{Float64}, Ptr{Float64}, Uint), x, y, n)
+    status != 0 && error("yepMath_Cos_V64f_V64f: error: ", status)
+    y
+end
+
+function tan!(y::AbstractVector{Float64}, x::AbstractVector{Float64})
+    assert(length(x) == length(y))
+    n = length(x)
+    const status = ccall( (:yepMath_Tan_V64f_V64f, "libyeppp"), Int32, (Ptr{Float64}, Ptr{Float64}, Uint), x, y, n)
+    status != 0 && error("yepMath_Tan_V64f_V64f: error: ", status)
+    y
+end
 
 end # module
