0fb34651ec5493acd82fe003fea7e70e1bf45dfc
Jonathan Goldfarb <jgoldfar@gmail.com>
2015-04-11 19:24:21 -0400

    Add preliminary support for evalpoly!

--- src/Yeppp.jl
+++ src/Yeppp.jl
@@ -52,6 +52,14 @@ function max!(res::Vector{Float64}, y::Vector{Float64}, x::Vector{Float64})
     res
 end
 
+function evalpoly!(res::Vector{Float64}, coef::Vector{Float64}, x::Vector{Float64})
+    assert(length(x) + 1 == length(coef))
+    n = length(coef)
+    const status = ccall( (:yepMath_EvaluatePolynomial_V64fV64f_V64f, "libyeppp"), Int32, (Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Uint), coef, x, res, n, n - 1)
+    status != 0 && error("yepMath_EvaluatePolynomial_V64fV64f_V64f: error: ", status)
+    res
+end
+
 function min!(res::Vector{Float64}, y::Vector{Float64}, x::Vector{Float64})
     assert(length(x) == length(y))
     n = length(x)
