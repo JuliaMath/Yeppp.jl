d196a1ea9910b28093418e4f6670f6ca44945136
Lanfeng Pan <panlanfeng@gmail.com>
2015-04-19 12:43:05 -0500

    add some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,23 @@ using Yeppp
 using Base.Test
 
 # write your own tests here
-@test 1 == 1
+x = rand(1000)
+y = rand(1000)
+z = randn(1000)
+res = rand(1000)
+
+@test Yeppp.add!(res, x, y) == x .+ y
+@test Yeppp.multiply!(res, x, y) == x .* y 
+@test Yeppp.subtract!(res, x, y) == x .- y 
+@test Yeppp.min!(res, x, y) == min(x, y)
+@test Yeppp.max!(res, x, y) == max(x, y)
+
+@test_approx_eq Yeppp.exp!(res, x) exp(x)
+@test_approx_eq Yeppp.log!(res, x) log(x)
+@test_approx_eq Yeppp.sin!(res, x) sin(x)
+@test_approx_eq Yeppp.cos!(res, x) cos(x)
+@test_approx_eq Yeppp.tan!(res, x) tan(x)
+
+@test_approx_eq Yeppp.sum(x) sum(x)
+@test_approx_eq Yeppp.sumabs(x) sumabs(x)
+@test_approx_eq Yeppp.sumabs2(x) sumabs2(x)
