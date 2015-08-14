f70672c967cd1621ebb0302dbab01d4cec24a6cc
Lanfeng Pan <panlanfeng@gmail.com>
2015-08-13 23:56:52 -0700

    choose cpu

--- deps/build.jl
+++ deps/build.jl
@@ -5,9 +5,30 @@ ver = "1.0.0"
 run(download_cmd("http://bitbucket.org/MDukhan/yeppp/downloads/yeppp-$ver.tar.bz2", "yeppp-$ver.tar.bz2"))
 run(unpack_cmd("yeppp-$ver.tar.bz2", ".", ".bz2", ".tar"))
 
-@windows_only run(`cp yeppp-$ver/binaries/windows/amd64/yeppp.dll yeppp-$ver/binaries/windows/amd64/yeppp.lib yeppp-$ver/binaries/windows/amd64/yeppp.pdb .`)
-@osx_only run(`cp yeppp-$ver/binaries/macosx/x86_64/libyeppp.dylib yeppp-$ver/binaries/macosx/x86_64/libyeppp.dylib.dSYM .`)
-@linux_only run(`cp yeppp-$ver/binaries/linux/x86_64/libyeppp.dbg yeppp-$ver/binaries/linux/x86_64/libyeppp.so .`)
+@windows_only begin
+    if Sys.WORD_SIZE == 64
+        wsize = "amd64"
+    else
+        wsize = "x86"
+    end
+    run(`cp yeppp-$ver/binaries/windows/$wsize/yeppp.dll yeppp-$ver/binaries/windows/$wsize/yeppp.lib yeppp-$ver/binaries/windows/$wsize/yeppp.pdb .`)
+end
+@osx_only begin
+    if Sys.WORD_SIZE == 64
+        wsize = "x86_64"
+    else
+        wsize = "x86"
+    end
+    run(`cp yeppp-$ver/binaries/macosx/$wsize/libyeppp.dylib yeppp-$ver/binaries/macosx/$wsize/libyeppp.dylib.dSYM .`)
+end
+@linux_only begin 
+    if Sys.WORD_SIZE == 64
+        wsize = "x86_64"
+    else
+        wsize = "i586"
+    end
+    run(`cp yeppp-$ver/binaries/linux/$wsize/libyeppp.dbg yeppp-$ver/binaries/linux/$wsize/libyeppp.so .`)
+end
 # provides(Binaries, URI("http://bitbucket.org/MDukhan/yeppp/downloads/yeppp-1.0.0.tar.bz2"))
 
 #@BinDeps.install 
