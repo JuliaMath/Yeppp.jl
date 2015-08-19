6e635e0a7ab492217114281766274e281744f1d3
U-IASTATE\pan <pan@ts3-stat.stat.iastate.edu>
2015-08-19 01:11:10 -0500

    work on windows

--- deps/build.jl
+++ deps/build.jl
@@ -2,14 +2,16 @@ using BinDeps
 using Compat
 @BinDeps.setup
 ver = "1.0.0"
-libyepppdep = library_dependency("libyeppp")
+@unix_only libyepppdep = library_dependency("libyeppp", alias=["yeppp"])
+@windows_only libyepppdep = library_dependency("yeppp")
+
 @windows_only begin
     if Sys.WORD_SIZE == 64
         wsize = "amd64"
     else
         wsize = "x86"
     end
-    provides(Binaries, URI("http://bitbucket.org/MDukhan/yeppp/downloads/yeppp-$ver.tar.bz2"), libyepppdep, unpacked_dir = "yeppp-$ver/binaries/windows/$wsize/", os = :windows)
+    provides(Binaries, URI("http://bitbucket.org/MDukhan/yeppp/downloads/yeppp-$ver.tar.bz2"), libyepppdep, unpacked_dir = "yeppp-$ver/binaries/windows/$wsize/", os = :Windows)
 end
 @osx_only begin
     if Sys.WORD_SIZE == 64
@@ -25,6 +27,8 @@ end
     else
         wsize = "i586"
     end
-    provides(Binaries, URI("http://bitbucket.org/MDukhan/yeppp/downloads/yeppp-$ver.tar.bz2"), libyepppdep, unpacked_dir = "yeppp-$ver/binaries/linux/$wsize/", os = :linux)
+    provides(Binaries, URI("http://bitbucket.org/MDukhan/yeppp/downloads/yeppp-$ver.tar.bz2"), libyepppdep, unpacked_dir = "yeppp-$ver/binaries/linux/$wsize/", os = :Linux)
 end
-@compat @BinDeps.install Dict(:libyeppp => :libyeppp)
+@unix_only @compat @BinDeps.install Dict(:libyeppp => :libyeppp)
+@windows_only @compat @BinDeps.install Dict(:yeppp => :yeppp)
+
