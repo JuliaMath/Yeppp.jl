9273207980d05b8bdbd736064db837b3b750de46
Lanfeng Pan <panlanfeng@gmail.com>
2018-01-15 20:54:56 -0500

    fix deprecation warning in testing file

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,14 +12,14 @@ for T in (Float32, Float64)
     @test Yeppp.subtract!(res, x, y) == x .- y
     @test Yeppp.multiply(x, y) == x .* y
     @test Yeppp.subtract(x, y) == x .- y
-    @test Yeppp.min!(res, x, y) == min(x, y)
-    @test Yeppp.max!(res, x, y) == max(x, y)
-    @test Yeppp.min(x, y) == min(x, y)
-    @test Yeppp.max(x, y) == max(x, y)
+    @test Yeppp.min!(res, x, y) == min.(x, y)
+    @test Yeppp.max!(res, x, y) == max.(x, y)
+    @test Yeppp.min(x, y) == min.(x, y)
+    @test Yeppp.max(x, y) == max.(x, y)
 
-    @test_approx_eq Yeppp.sum(x) sum(x)
-    @test_approx_eq Yeppp.sumabs2(x) sumabs2(x)
-    @test_approx_eq Yeppp.dot(x, y) dot(x, y)
+    @test Yeppp.sum(x) ≈ sum(x)
+    @test Yeppp.sumabs2(x) ≈ sum(abs2, x)
+    @test Yeppp.dot(x, y) ≈ dot(x, y)
 
 end
 
@@ -28,13 +28,12 @@ y = rand(1000)
 z = randn(1000)
 res = rand(1000)
 
-@test_approx_eq Yeppp.exp!(res, x) exp(x)
-@test_approx_eq Yeppp.log!(res, x) log(x)
-@test_approx_eq Yeppp.sin!(res, x) sin(x)
-@test_approx_eq Yeppp.cos!(res, x) cos(x)
-@test_approx_eq Yeppp.tan!(res, x) tan(x)
+@test Yeppp.exp!(res, x) ≈ exp.(x)
+@test Yeppp.log!(res, x) ≈ log.(x)
+@test Yeppp.sin!(res, x) ≈ sin.(x)
+@test Yeppp.cos!(res, x) ≈ cos.(x)
+@test Yeppp.tan!(res, x) ≈ tan.(x)
 
-@test_approx_eq Yeppp.sum(x) sum(x)
-@test_approx_eq Yeppp.sumabs(x) sumabs(x)
-@test_approx_eq Yeppp.sumabs2(x) sumabs2(x)
-
+@test Yeppp.sum(x) ≈ sum(x)
+@test Yeppp.sumabs(x) ≈ sum(abs, x)
+@test Yeppp.sumabs2(x) ≈ sum(abs2, x)
